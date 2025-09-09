beffb70ac9ed7b8b491252cfb046fd90040f66bd
namlev <mikipatkowski@protonmail.com>
2025-09-09 14:50:40 +0200

    Implement /login redirect middleware

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const privateRoutes = ['/dashboard', '/profile', '/admin'];
+
+function getPlatformIdHeader() {
+  const platformId = '4e1c791d-c481-4f9a-9eff-b701c2875c5f';
+
+  return {
+    'x-platform-id': platformId,
+  };
+}
+
+export function middleware(request: NextRequest) {
+  // Mock tenant detection
+  const platformHeader = getPlatformIdHeader();
+  const { pathname } = request.nextUrl;
+
+  const isPrivateRoute = privateRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+  const jwt = request.cookies.get('jwt')?.value;
+
+  if (isPrivateRoute && !jwt) {
+    const loginUrl = new URL('/login', request.url);
+    loginUrl.searchParams.set('redirect', pathname);
+    return NextResponse.redirect(loginUrl, {
+      headers: platformHeader,
+    });
+  }
+
+  return NextResponse.next({
+    headers: platformHeader,
+  });
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
