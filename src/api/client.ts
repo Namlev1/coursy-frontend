7b8049b5f846f6d458d3d7a6ff5906d293be991c
namlev <mikipatkowski@protonmail.com>
2025-09-12 16:44:20 +0200

    Add axios

---
+++ src/api/client.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
+
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to add JWT token
+apiClient.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get('jwt');
+    if (token) {
+      config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${token}`,
+      };
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default apiClient;
